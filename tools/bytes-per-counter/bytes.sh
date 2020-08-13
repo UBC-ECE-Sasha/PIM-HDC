e31f70ff11e45a14b4fc0e40dc86bc1b15e16f85
John Ramsden <john@ubc.ramsdenj.ca>
2020-08-12 18:15:53 -0700

    Add slowest / fastest ratio
    Relates to #26

--- tools/bytes-per-counter/bytes.sh
+++ tools/bytes-per-counter/bytes.sh
@@ -8,10 +8,17 @@
 bytes="${1:-1905024}"
 file="${2:-"data/large8-data.bin"}"
 
-count=$(./pim_hdc -d -i "${file}" | \
+out=$(./pim_hdc -d -i "${file}" | \
 grep 'Tasklet [0-9]*: completed in' | \
-grep -o -P '(?<=in )[0-9]*(?= cycles)' | \
-awk '{ sum+=$1} END {print sum}')
+grep -o -P '(?<=in )[0-9]*(?= cycles)')
+
+count=$(echo "${out}" | awk '{ sum+=$1} END {print sum}')
+
+sorted=$(echo "${out}" | sort)
+largest=$(echo "${sorted}" | head -1)
+smallest=$(echo "${sorted}" | tail -1)
+
+ratio=$(echo "${largest} / ${smallest}" | bc -l)
 
 cpb=$(echo "${count} / ${bytes}" | bc -l)
 
@@ -19,6 +26,9 @@ echo "count: ${count}"
 echo "bytes: ${bytes}"
 
 echo "${count} / ${bytes} = ${cpb}"
+
+echo "slowest / fastest ratio"
+echo "${largest} / ${smallest} = ${ratio}"
 
 # CYCLES / BYTE =
 #     654599782096 / 1905024 = 343617.60381811462742726600
